fc3ea7f15aa7a85eaa8b366cb2bd1eeec1b14b9c
Maria M <prov21_1@hotmail.com>
2021-03-23 20:26:31 -0700

    add validation

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
-    },
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput)  {
+        return true;
+      } else {
+        console.log('Please enter your name!');
+        return false;
+      }
+    }
+  },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput)  {
+        return true;
+      } else {
+        console.log('Please enter your GitHub Username!');
+        return false;
+      }
+    }
     },
     {
       type: 'input',
@@ -34,12 +50,28 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project? (Required)',
+      validate: nameInput => {
+        if (nameInput)  {
+        return true;
+      } else {
+        console.log('Please enter your project name!');
+        return false;
+      }
+    }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: descriptionInput => {
+        if (descriptionInput)  {
+        return true;
+      } else {
+        console.log('Please enter a description!');
+        return false;
+      }
+    }
     },
     {
       type: 'checkbox',
@@ -51,6 +83,14 @@ Add a New Project
       type: 'input',
       name: 'link',
       message: 'Enter the GitHub link to your project. (Required)'
+      validate: linkInput => {
+        if (linkInput)  {
+        return true;
+      } else {
+        console.log('Please enter the GitHub link to your project!');
+        return false;
+      }
+    }
     },
     {
       type: 'confirm',
